341932e51c6be84799996274f490803bedbb12b0
Samantha DeSmul <samanthadesmul@yahoo.com>
2019-04-03 17:17:03 -0400

    added a title and header

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samantha
+ * Date: 4/3/2019
+ * Time: 2:13 PM
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair program</title>
+</head>
+<body>
+<h1>Pair Program 1</h1>
+
+</body>
+</html>
